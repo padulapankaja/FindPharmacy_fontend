dd951726bf642cfb06b13ba64fe192da6f3b53db
lucidex <padulaguruge@gmail.com>
2020-03-29 11:13:19 +0530

    add mohe and clear consoles

--- src/Components/allshops.js
+++ src/Components/allshops.js
@@ -12,7 +12,8 @@ import NewsApi from '../NewsApi'
 import Oneshop from './oneshop'
 import Loading from "../Components/loading"
 
-
+import p1 from '../Images/main-qimg-7bb4172052a760365f5a1c35142eaea3.png'
+import p2 from '../Images/pharmacy_app.png'
 
 
 
@@ -29,6 +30,7 @@ class AllShops extends Component {
             searchKey: '',
             searchedArray: [],
             loading: false,
+            image:true
         };
 
 
@@ -36,31 +38,31 @@ class AllShops extends Component {
     }
 
 
-    async componentWillMount() {
-        this.setState({
-            loading: true,
-        })
-        await axios.get(`${NewsApi.server}${NewsApi.port}${NewsApi.api.all}`).then(res => {
-            console.log("Success Send");
-            console.log(res);
+    // async componentWillMount() {
+    //     this.setState({
+    //         loading: true,
+    //     })
+    //     await axios.get(`${NewsApi.server}${NewsApi.port}${NewsApi.api.all}`).then(res => {
+    //         console.log("Success Send");
+    //         console.log(res);
 
-            this.setState({
-                allshops: res.data
-            })
+    //         this.setState({
+    //             allshops: res.data
+    //         })
 
-        }).catch(function (error) {
-            console.log(error);
-        })
+    //     }).catch(function (error) {
+    //         console.log(error);
+    //     })
 
-        await console.log(this.state.allshops);
+    //     await console.log(this.state.allshops);
 
 
-        this.ListofAllPharmacy()
+    //     this.ListofAllPharmacy()
 
-        this.setState({
-            loading: false,
-        })
-    }
+    //     this.setState({
+    //         loading: false,
+    //     })
+    // }
 
 
     ListofAllPharmacy() {
@@ -85,9 +87,11 @@ class AllShops extends Component {
     async  Onsubmit(e) {
         this.setState({
             loading: true,
+            image:false
+
         })
-        console.log("Keyword -----");
-        console.log(this.state.searchKey);
+        // console.log("Keyword -----");
+        // console.log(this.state.searchKey);
 
         e.preventDefault();
         var search = this.state.searchKey
@@ -97,8 +101,8 @@ class AllShops extends Component {
         }
 
         await axios.post(`${NewsApi.server}${NewsApi.port}${NewsApi.api.search}`, { keyWor }).then(res => {
-            console.log("Keyqord");
-            console.log(res);
+            // console.log("Keyqord");
+            // console.log(res);
 
             this.setState({
                 searchedArray: res.data
@@ -108,11 +112,13 @@ class AllShops extends Component {
             console.log(error);
         })
 
-        await console.log(this.state.searchedArray);
+        // await console.log(this.state.searchedArray);
 
         await this.ListofSearchPharmacy()
         this.setState({
             loading: false,
+            // searchKey:''
+            image:false
         })
     }
 
@@ -154,6 +160,19 @@ class AllShops extends Component {
                 </div>
                 <div className="row">
 
+                <div className="col-md-12">
+                <img  style={{display:this.state.image == true ? 'block' : 'none'}} className="img-fluid mb-4 mt-4"  src={p1}/>
+                {/* <h5 className="title" style={{fontWeight:'900'}}> How to protect yourself from the coronavirus</h5>
+                <p>Wash your hands</p>
+                <p>Stay home</p>
+                <p>Boost your immune system</p> */}
+
+                </div>
+                
+              
+                {/* <div className="col-md-6">
+                <img src={p2}/>
+                </div> */}
                     <div className="all_pharam" style={{ display: this.state.searchedArray.length <= 0 ? 'none' : 'block' }} >
                         <h3 className="all_pharam_title" style={{ display: this.state.searchedArray.length <= 0 ? 'none' : 'block' }} >{this.state.searchedArray.length} &nbsp; Pharmacies  in &nbsp; {this.state.searchKey}&nbsp;  MOH Area </h3>
                     </div>
@@ -161,13 +180,13 @@ class AllShops extends Component {
                     {this.ListofSearchPharmacy()}
 
                 </div>
-                <div className="row mt-2">
+                {/* <div className="row mt-2">
                     <div className="all_pharam">
                         <h3>All Pharmacies</h3>
                     </div>
                     {this.ListofAllPharmacy()}
 
-                </div>
+                </div> */}
             </div>
         );
     }
